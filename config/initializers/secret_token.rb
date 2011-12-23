8d607cecbb5b7e0ed9d74923c3d6a2ac9a52d504
odysseus <odysseus@cosmosgame.org>
2011-12-23 04:46:44 -0800

    Initial check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f84a9e59540a495714d3422c17e3d02e49410837ee00fcf4ae818fc1477d64620e14afcc278324ea708a3268fbeb3b8848821f7588de1ac59bab4636bae16995'
